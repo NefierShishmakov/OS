6f6f0f1006f63c8a935d66459e2033d7e5495ccf
Reifenier <73580080+NefierShishmakov@users.noreply.github.com>
2022-09-20 20:29:47 +0700

    Add files via upload

---
+++ lab4.c
@@ -0,0 +1,59 @@
+#include <pthread.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <errno.h>
+
+#define SUCCESS 0
+#define FAILURE 1
+
+#define SEC_TO_SLEEP 2
+#define TRUE 1
+
+typedef unsigned long long ull;
+
+void *print_lines() {
+    ull i = 1;
+
+    while (TRUE) {
+        printf("Hello, I\'m your %lld friend\n", i);
+        ++i;
+    }
+}
+
+int main(void) {
+    pthread_t pthread_id;
+
+    int create_status = pthread_create(&pthread_id, NULL, print_lines, NULL);
+
+    if (create_status != SUCCESS) {
+        errno = create_status;
+        perror("pthread_create");
+        return FAILURE;
+    }
+
+    sleep(SEC_TO_SLEEP);
+
+    int cancel_status = pthread_cancel(pthread_id);
+
+    if (cancel_status != SUCCESS) {
+        errno = cancel_status;
+        perror("pthread_cancel");
+        return FAILURE;
+    }
+
+    void *res;
+
+    int join_status = pthread_join(pthread_id, &res);
+
+    if (join_status != SUCCESS) {
+        errno = join_status;
+        perror("pthread_join");
+        return FAILURE;
+    }
+
+    if (res == PTHREAD_CANCELED) {
+        printf("The child pthread was cancelled\n");
+    }
+
+    pthread_exit(NULL);
+}
---
+++ lab5.c
@@ -0,0 +1,69 @@
+#include <pthread.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <errno.h>
+
+#define SUCCESS 0
+#define FAILURE 1
+
+#define SEC_TO_SLEEP 2
+#define TRUE 1
+
+#define EXECUTE 1
+
+void print_goodbye() {
+    printf("Goodbye\n");
+}
+
+typedef unsigned long long ull;
+
+void *print_lines() {
+    ull i = 1;
+
+    pthread_cleanup_push(print_goodbye, NULL);
+
+    while (TRUE) {
+        printf("Hello, I\'m your %lld friend\n", i);
+        ++i;
+    }
+
+    pthread_cleanup_pop(EXECUTE);
+}
+
+int main(void) {
+    pthread_t pthread_id;
+
+    int create_status = pthread_create(&pthread_id, NULL, print_lines, NULL);
+
+    if (create_status != SUCCESS) {
+        errno = create_status;
+        perror("pthread_create");
+        return FAILURE;
+    }
+
+    sleep(SEC_TO_SLEEP);
+
+    int cancel_status = pthread_cancel(pthread_id);
+
+    if (cancel_status != SUCCESS) {
+        errno = cancel_status;
+        perror("pthread_cancel");
+        return FAILURE;
+    }
+
+    void *res;
+
+    int join_status = pthread_join(pthread_id, &res);
+
+    if (join_status != SUCCESS) {
+        errno = join_status;
+        perror("pthread_join");
+        return FAILURE;
+    }
+
+    if (res == PTHREAD_CANCELED) {
+        printf("The child pthread was cancelled\n");
+    }
+
+    pthread_exit(NULL);
+}

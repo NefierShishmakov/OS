74624feb9a9bbb2207191d63dbaa534ae06028cb
mrx <i.shishmakov@g.nsu.ru>
2022-10-03 21:17:41 +0700

    updated

--- lab3/c/lab3.c
+++ lab3/c/lab3.c
@@ -5,75 +5,118 @@
 
 #define SUCCESS 0
 #define ERROR -1
-#define TOTAL_LINES 24
+#define TOTAL_LINES_NUM 24
 #define PTHREADS_NUM 4
-#define LINES_PER_THREAD (TOTAL_LINES / PTHREADS_NUM)
 
 typedef struct lines_t {
     char **strings;
     int pthread_num;
+    int strings_num;
 } lines_t;
 
-void init_lines_t(lines_t *arg, char **start_point, const int pthread_num) {
+void init_lines_t(lines_t *arg, char **start_point, const int pthread_num, const int strings_num) {
     arg->strings = start_point;
     arg->pthread_num = pthread_num;
+    arg->strings_num = strings_num;
 }
 
 void *print_lines(void *arg) {
     lines_t *lines = (lines_t *)arg;
 
-    for (int i = 0; i < LINES_PER_THREAD; ++i) {
-        printf("%d thread: %s\n", lines->pthread_num, lines->strings[i]);
+    for (int i = 0; i < lines->strings_num; ++i) {
+        printf("Messsage from pthread with %d id: %s\n", lines->pthread_num, lines->strings[i]);
     }
 
     pthread_exit(NULL);
 }
 
-static char *strings[TOTAL_LINES] = {
-    "HELLO",
-    "HI",
-    "OHHH NOO",
-    "PRINT",
-    "LOKIK",
-    "REST",
-    "APPLE",
-    "BANANA",
-    "HALAYVA",
-    "NI",
-    "HU",
-    "UUUUU",
-    "AAAAA",
-    "SSSSS",
-    "DDDDD",
-    "YYYYY",
-    "HHHHH",
-    "JJJJJ",
-    "KKKKK",
-    "PRINTED",
-    "OHHHH YES",
-    "STATIS",
-    "STATICAL",
-    "TRAGIC",
-};
+int pthreads_creation(pthread_t *pthreads_ids, char **lines, int *lines_distribution) {
+    for (int i = 0, idx = 0; i < PTHREADS_NUM; ++i, idx += lines_distribution[i]) {
+        lines_t pthread_lines;
+        init_lines_t(&pthread_lines, &lines[idx], i + 1, lines_distribution[i]);
 
-static lines_t lines[PTHREADS_NUM];
-
-int main(void) {
-    pthread_t pthreads_ids[PTHREADS_NUM];
-
-    for (int i = 0; i < PTHREADS_NUM; ++i) {
-        init_lines_t(&lines[i], &strings[i * LINES_PER_THREAD], i);
-
-        int create_status = pthread_create(&pthreads_ids[i], NULL, print_lines, &lines[i]);
+        int create_status = pthread_create(&pthreads_ids[i], NULL, print_lines, &pthread_lines);
 
         if (create_status != SUCCESS) {
             errno = create_status;
             perror("pthread_create");
-
-            return EXIT_FAILURE;
+            return ERROR;
         }
     }
 
-    pthread_exit(NULL);
+    return SUCCESS;
+}
+
+int pthreads_join(pthread_t *pthreads_ids) {
+    for (int i = 0; i < PTHREADS_NUM; ++i) {
+        int join_status = pthread_join(pthreads_ids[i], NULL);
+
+        if (join_status != SUCCESS) {
+            errno = join_status;
+            perror("pthread_join");
+            return ERROR;
+        }
+    }
+
+    return SUCCESS;
+}
+
+void init_lines_distribution(int *lines_distribution) {
+    int lines_per_pthread = TOTAL_LINES_NUM / PTHREADS_NUM;
+    int remainder = TOTAL_LINES_NUM % PTHREADS_NUM;
+    lines_distribution[PTHREADS_NUM - 1] += remainder;
+
+    for (int i = 0; i < PTHREADS_NUM; ++i) {
+        lines_distribution[i] += lines_per_pthread;
+    }
+} 
+
+int main(void) {
+    pthread_t pthreads_ids[PTHREADS_NUM];
+    
+    char *lines[TOTAL_LINES_NUM] = {
+        "HELLO",
+        "LALALAL",
+        "PRIVET",
+        "RUSSIA",
+        "TODAY",
+        "APPLE",
+        "BANANA",
+        "RESERVED",
+        "SUCCESS",
+        "ADMIRAL",
+        "ORANGE",
+        "RPH",
+        "NOBODY",
+        "WEEk",
+        "DAY",
+        "CIRCLE",
+        "FUTURE",
+        "PPPPPP",
+        "RRRRR",
+        "MAN",
+        "WOMAN",
+        "RESOURCE",
+        "TRYING",
+        "POP"
+    };
+
+    int lines_distribution[PTHREADS_NUM] = {0};
+
+    init_lines_distribution(lines_distribution);
+    
+    int pthreads_creation_status = pthreads_creation(pthreads_ids, lines, lines_distribution);
+
+    if (pthreads_creation_status == ERROR) {
+        return EXIT_FAILURE;
+    }
+    
+    int pthreads_join_status = pthreads_join(pthreads_ids);
+
+    if (pthreads_join_status == ERROR) {
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
 }
 

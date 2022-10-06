52f193afda2145d057086919dbef8f9ba596b997
mrx <i.shishmakov@g.nsu.ru>
2022-10-07 01:23:45 +0700

    started doing lab6

--- lab6/c/lab6.c
+++ lab6/c/lab6.c
@@ -1,13 +1,16 @@
 #include <pthread.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 #define MAX_STRINGS_NUM 100
+#define MIN_STRINGS_NUM 1
 
 int main(int argc, char **argv) {
-    if (argc > MAX_STRINGS_NUM) {
-        fprintf(stderr, "Too many strings ");
+    if (argc < MIN_STRINGS_NUM || argc > MAX_STRINGS_NUM) {
+        fprintf(stderr, "Strings number should be more or equal %d and less or equal %d\n", MIN_STRINGS_NUM, MAX_STRINGS_NUM);
         return EXIT_FAILURE;
     }
 
     return EXIT_SUCCESS;
 }
+

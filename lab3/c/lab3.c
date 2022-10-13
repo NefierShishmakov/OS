2c528bb56cd9258dba5acf31c20f3646aacdbac4
mrx <i.shishmakov@g.nsu.ru>
2022-10-13 17:17:05 +0700

    changed lab3

--- lab3/c/lab3.c
+++ lab3/c/lab3.c
@@ -31,17 +31,19 @@ void *print_lines(void *arg) {
 }
 
 int pthreads_join(pthread_t *pthreads_ids) {
+    int status = SUCCESS;
+
     for (int i = 0; i < PTHREADS_NUM; ++i) {
         int join_status = pthread_join(pthreads_ids[i], NULL);
 
         if (join_status != SUCCESS) {
             errno = join_status;
             perror("pthread_join");
-            return ERROR;
+            status = ERROR;
         }
     }
 
-    return SUCCESS;
+    return status;
 }
 
 void init_lines_distribution(int *lines_distribution) {

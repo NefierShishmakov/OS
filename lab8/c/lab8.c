2f35b376a1da84cd9ce9e3d653527372b767da32
mrx <i.shishmakov@g.nsu.ru>
2022-10-19 18:56:54 +0700

    changed lab8

--- lab8/c/lab8.c
+++ lab8/c/lab8.c
@@ -3,11 +3,13 @@
 #include <stdlib.h>
 #include <pthread.h>
 
-#define STEPS_NUM 698377680
 #define DEFAULT_ARGC_NUM 2
 #define PTHREADS_NUM_ARGC_INDEX 1
+#define STEPS_NUM_ARGC_INDEX 2
+
 #define MAX_PTHREADS_NUM 256
 #define MIN_PTHREADS_NUM 1
+#define MIN_STEPS_NUM 1
 
 #define SUCCESS 0
 #define SUCCESS_CONVERTION_STATUS 0
@@ -20,9 +22,9 @@ typedef struct info {
     double partial_sum;
 } info;
 
-void init_pthreads_args(info *pthreads_args, const int pthreads_num) {
-    int pthread_steps_num = STEPS_NUM / pthreads_num;
-    int remainder = STEPS_NUM % pthreads_num;
+void init_pthreads_args(info *pthreads_args, const int pthreads_num, const int steps_num) {
+    int pthread_steps_num = steps_num / pthreads_num;
+    int remainder = steps_num % pthreads_num;
 
     for (int i = 0, start_point = 0; i < pthreads_num; start_point += pthread_steps_num, ++i) {
         pthreads_args[i].start_point = start_point;
@@ -46,8 +48,8 @@ void *get_partial_sum(void *arg) {
     pthread_exit(&pthread_info->partial_sum);
 }
 
-int args_analyzer(int argc, char **argv, int *result_pthreads_num) {
-    if (argc != DEFAULT_ARGC_NUM) {
+int args_analyzer(int argc, char **argv, int *result_pthreads_num, int *result_steps_num) {
+    if ((argc - 1) != DEFAULT_ARGC_NUM) {
         return ERROR;
     }
 
@@ -58,18 +60,28 @@ int args_analyzer(int argc, char **argv, int *result_pthreads_num) {
         perror("strtol");
         return ERROR;
     }
+
+    errno = SUCCESS_CONVERTION_STATUS;
+    int steps_num = (int)strtol(argv[STEPS_NUM_ARGC_INDEX], NULL, 10);
+
+    if (errno != SUCCESS_CONVERTION_STATUS) {
+        perror("strtol");
+        return ERROR;
+    }
     
-    if (pthreads_num < MIN_PTHREADS_NUM || pthreads_num > MAX_PTHREADS_NUM) {
+    if ((pthreads_num < MIN_PTHREADS_NUM || pthreads_num > MAX_PTHREADS_NUM) 
+            || (steps_num < MIN_STEPS_NUM)) {
         return ERROR;
     }
     
     *result_pthreads_num = pthreads_num;
+    *result_steps_num = steps_num;
 
     return SUCCESS;
 }
 
 void print_error_message() {
-    fprintf(stderr, "Usage: arg1 - number of threads\n");
+    fprintf(stderr, "Usage: arg1 - number of threads, arg2 - number of steps\n");
 }
 
 void join_created_threads(pthread_t *pthreads_ids, const int last_created_thread_idx) {
@@ -83,9 +95,9 @@ void join_created_threads(pthread_t *pthreads_ids, const int last_created_thread
     }
 }
 
-int calculate_pi(double *pi, const int pthreads_num) {
+int calculate_pi(double *pi, const int pthreads_num, const int steps_num) {
     info pthreads_args[pthreads_num];
-    init_pthreads_args(pthreads_args, pthreads_num);
+    init_pthreads_args(pthreads_args, pthreads_num, steps_num);
     
     pthread_t pthreads_ids[pthreads_num];
 
@@ -134,8 +146,9 @@ void print_result(const double *pi) {
 
 int main(int argc, char **argv) {
     int pthreads_num;
+    int steps_num;
 
-    int status = args_analyzer(argc, argv, &pthreads_num);
+    int status = args_analyzer(argc, argv, &pthreads_num, &steps_num);
 
     if (status != SUCCESS) {
         print_error_message();
@@ -143,7 +156,7 @@ int main(int argc, char **argv) {
     }
     
     double pi = 0;
-    status = calculate_pi(&pi, pthreads_num);
+    status = calculate_pi(&pi, pthreads_num, steps_num);
 
     if (status != SUCCESS) {
         return EXIT_FAILURE;

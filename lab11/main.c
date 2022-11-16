7e896ad8e87bd74eff2bb036c03acfea070e1d27
mrx <i.shishmakov@g.nsu.ru>
2022-11-16 14:53:22 +0700

    finished lab11

--- lab11/main.c
+++ lab11/main.c
@@ -9,8 +9,10 @@
 #define SUCCESS 0
 #define ERROR (-1)
 
-#define PTHREADS_NUM 2
-#define MUTEXES_NUM (PTHREADS_NUM + 1)
+#define PARENT_ID 0
+#define CHILD_ID 2
+
+#define MUTEXES_NUM 3
 
 pthread_mutex_t mutexes[MUTEXES_NUM];
 
@@ -18,7 +20,8 @@ bool mutex_is_locked = false;
 
 typedef struct function_args {
     const char *name;
-    int num;
+    int iter_num;
+    int id;
 } function_args;
 
 void handle_error(int status, const char *error_reason) {
@@ -71,29 +74,45 @@ int init_mutexes() {
 void *start_routine(void *arg) {
     function_args *args = (function_args *)arg;
 
-    for (int i = 1; i <= args->num; ++i) {
-        printf("The %s line is printed\n", args->name);
+    int id = args->id;
+
+    if (!mutex_is_locked) {
+        pthread_mutex_lock(&mutexes[id % MUTEXES_NUM]);
+        mutex_is_locked = true;
     }
+
+    int next_id = 0;
+
+    for (int i = 1; i <= args->iter_num; ++i) {
+        next_id = (id + 1) % MUTEXES_NUM;
+        pthread_mutex_lock(&mutexes[next_id]);
+        printf("The %s line is printed\n", args->name);
+        pthread_mutex_unlock(&mutexes[id]);
+        id = next_id;
+    }
+
+    pthread_mutex_unlock(&mutexes[id % MUTEXES_NUM]);
 
     return NULL;
 }
 
 int main(void) {
-    pthread_t pthread_id[PTHREADS_NUM - 1];
+    pthread_t pthread_id;
     int status = init_mutexes();
 
     if (status == ERROR) {
         return EXIT_FAILURE;
     }
 
-    function_args child = {"child", LINES_NUM};
-    function_args parent = {"parent", LINES_NUM};
+    function_args child = {"child", LINES_NUM, CHILD_ID};
+    function_args parent = {"parent", LINES_NUM, PARENT_ID};
 
     pthread_mutex_lock(&mutexes[0]);
 
     status = pthread_create(&pthread_id, NULL, start_routine, &child);
 
     if (status != SUCCESS) {
+        pthread_mutex_unlock(&mutexes[0]);
         handle_error(status, "pthread_create");
         destroy_mutexes(MUTEXES_NUM - 1);
 
@@ -103,6 +122,8 @@ int main(void) {
     while (!mutex_is_locked) {
         sched_yield();
     }
+
+    start_routine(&parent);
 
     status = pthread_join(pthread_id, NULL);
 
@@ -116,3 +137,4 @@ int main(void) {
     destroy_mutexes(MUTEXES_NUM - 1);
     return EXIT_SUCCESS;
 }
+

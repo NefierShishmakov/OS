b9a1ae5a54b89e05e35b3735cc21881ddbd25fa2
mrx <i.shishmakov@g.nsu.ru>
2022-11-17 23:45:46 +0700

    started doing lab14 and created explanation for it

--- lab14/c/lab14.c
+++ lab14/c/lab14.c
@@ -1,4 +1,150 @@
-int main(void)
-{
-    return 0;
+#include <stdio.h>
+//#include <stdbool.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <errno.h>
+#include <semaphore.h>
+
+#define LINES_NUM 10
+#define SUCCESS 0
+#define ERROR (-1)
+
+#define PARENT_ID 0
+#define CHILD_ID 2
+
+#define FIRST_SEM_START_VALUE 0
+#define SECOND_SEM_START_VALUE 1
+
+#define SEM_NUM 2
+
+sem_t semaphores[SEM_NUM];
+
+//bool mutex_is_locked = false;
+
+typedef struct function_args {
+    const char *name;
+    int iter_num;
+    int id;
+} function_args;
+
+void handle_error(int status, const char *error_reason) {
+    errno = status;
+    perror(error_reason);
 }
+
+int init_semaphore(int sem_num, int sem_start_value) {
+    int status = sem_init(&semaphores[sem_num], 0, sem_start_value);
+
+    if (status != SUCCESS) {
+        handle_error(status, "sem_init");
+        return ERROR;
+    }
+
+    return SUCCESS;
+}
+
+int destroy_semaphore(int sem_num) {
+    int status = sem_destroy(&semaphores[sem_num]);
+
+    if (status != SUCCESS) {
+        handle_error(status, "sem_destroy");
+        return ERROR;
+    }
+
+    return SUCCESS;
+}
+
+int destroy_semaphores(int last_index_of_initialised_semaphore) {
+    int status;
+
+    for (int i = 0; i <= last_index_of_initialised_semaphore; ++i) {
+        status = destroy_semaphore(i);
+    }
+
+    return status;
+}
+
+int init_semaphores(int *sem_values) {
+    int status;
+    
+    for (int i = 0; i < SEM_NUM; ++i) {
+        status = init_semaphore(i, sem_values[i]);
+
+        if (status != SUCCESS) {
+            destroy_semaphores(i - 1);
+            return ERROR;
+        }
+    }
+
+    return SUCCESS;
+}
+
+void *start_routine(void *arg) {
+    function_args *args = (function_args *)arg;
+
+    return NULL;
+}
+
+int main(void) {
+    pthread_t pthread_id;
+    int sem_values[SEM_NUM] = {FIRST_SEM_START_VALUE, SECOND_SEM_START_VALUE};
+
+    int status = init_semaphores(sem_values);
+
+    if (status == ERROR) {
+        return EXIT_FAILURE;
+    }
+
+    function_args child = {"child", LINES_NUM, CHILD_ID};
+    function_args parent = {"parent", LINES_NUM, PARENT_ID};
+
+    status = pthread_create(&pthread_id, NULL, start_routine, &child);
+
+    if (status != SUCCESS) {
+        handle_error(status, "pthread_create");
+        destroy_semaphores(SEM_NUM - 1);
+        return EXIT_FAILURE;
+    }
+
+    //while (!mutex_is_locked) {}
+
+    start_routine(&parent);
+
+    status = pthread_join(pthread_id, NULL);
+
+    if (status != SUCCESS) {
+        handle_error(status, "pthread_join");
+        destroy_semaphores(SEM_NUM - 1);
+        return EXIT_FAILURE;
+    }
+    
+    status = destroy_semaphores(SEM_NUM - 1);
+
+    if (status != SUCCESS) {
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
+}
+
+
+/*int id = args->id;
+
+    if (!mutex_is_locked) {
+        lock_mutex(id % MUTEXES_NUM);
+        mutex_is_locked = true;
+    }
+
+    int next_id = 0;
+
+    for (int i = 1; i <= args->iter_num; ++i) {
+        next_id = (id + 1) % MUTEXES_NUM;
+        lock_mutex(next_id);
+        printf("The %s %d line is printed\n", args->name, i);
+        unlock_mutex(id);
+        id = next_id;
+    }
+
+    unlock_mutex(id % MUTEXES_NUM);
+    */
+

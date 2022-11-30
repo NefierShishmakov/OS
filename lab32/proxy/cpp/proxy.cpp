4d5f78775c12078df51f3f1ec6b91c79ddcbd874
NefierShishmakov <i.shishmakov@g.nsu.ru>
2022-11-30 22:43:05 +0700

    'added some info to lab32'

--- lab32/proxy/cpp/proxy.cpp
+++ lab32/proxy/cpp/proxy.cpp
@@ -2,6 +2,7 @@
 #include <pthread.h>
 #include "../hpp/proxy_utils.h"
 #include "../hpp/utils.h"
+#include <arpa/inet.h>
 
 
 
@@ -16,6 +17,41 @@ int main(int argc, char **argv) {
 
     if (proxy_fd == Constants::ERROR) {
         return EXIT_FAILURE;
+    }
+
+    while (true) {
+        struct sockaddr_in client_addr;
+        int clilen = sizeof(client_addr);
+
+        int client_fd = accept(proxy_fd, (struct sockaddr *)&client_addr, (socklen_t *)&clilen);
+
+        if (client_fd == Constants::ERROR) {
+            perror("accept");
+            int close_status = close(client_fd);
+
+            if (close_status == Constants::ERROR) {
+                perror("\'close\' method of socket client");
+            }
+        }
+
+        char *client_ip = inet_ntoa(client_addr.sin_addr);
+
+        std::cout << "New client with ip - " << "(" << client_ip << ")" << "has been connected" << std::endl;
+
+        char buffer[257] = {0};
+
+        std::cout << "Server started receiving data from the client" << std::endl;
+        int read_status = read(client_fd, buffer, 256);
+        std::cout << "Server ended receiving data from the client" << std::endl;
+
+        if (read_status == Constants::ERROR) {
+            perror("read");
+        }
+
+        std::cout << "The data received from the client" << std::endl;
+        std::cout << buffer << std::endl;
+
+        close(client_fd);
     }
 
     int close_proxy_fd_status = close(proxy_fd);
